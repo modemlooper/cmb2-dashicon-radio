9dfa026e8edbbadc167a333e152a33e0923aea83
modemlooper <modemlooper@gmail.com>
2016-10-19 08:51:37 -0700

    add Justins code review edits :)

--- dashicon-radio-field.php
+++ dashicon-radio-field.php
@@ -20,10 +20,10 @@ License: GPL-2.0+
  */
 function ml_cmb2_render_dashicon_radio_callback( $field, $escaped_value, $object_id, $object_type, $field_type_object ) {
 
-	if ( 'dashicon_radio' === $field->args['type'] ) {
-		$field->args['options'] = ml_cmb2_dashicons_return_array();
-	}
+	add_filter( 'cmb2_list_input_attributes', 'ml_cmb2_dashicon_radio_attributes', 10, 4 );
+	$field->args['options'] = ml_cmb2_dashicons_return_array();
 	echo $field_type_object->radio();
+	remove_filter( 'cmb2_list_input_attributes', 'ml_cmb2_dashicon_radio_attributes', 10, 4 );
 
 }
 add_action( 'cmb2_render_dashicon_radio', 'ml_cmb2_render_dashicon_radio_callback', 10, 5 );
@@ -39,12 +39,8 @@ add_action( 'cmb2_render_dashicon_radio', 'ml_cmb2_render_dashicon_radio_callbac
  */
 function ml_cmb2_dashicon_radio_attributes( $args, $defaults, $field, $cmb ) {
 
-	if ( 'dashicon_radio' === $field->args['type'] ) {
-		foreach ( cmb2_dashicons_return_array() as $dashicon => $name ) {
-			if ( $dashicon === $args['value'] ) {
-				$args['label'] = '<span class="dashicons ' . $dashicon . '"></span> ' . $name ;
-			}
-		}
+	if ( $args['value'] ) {
+		$args['label'] = '<span class="dashicons ' . $args['value'] . '"></span> ' . $args['label'];
 	}
 	return $args;
 }
@@ -65,7 +61,10 @@ function ml_cmb2_dashicon_radio_css() {
 		}
 		.cmb-type-dashicon-radio .cmb2-radio-list {
 			padding: 10px;
-
+		}
+		.cmb-type-dashicon-radio li {
+			display: inline-block;
+			min-width: 222px;
 		}
 	</style>
 	<?php
